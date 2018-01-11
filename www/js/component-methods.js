7a0444106d98e686428f323ed94c8974319317e3
nbriz <nickbriz@gmail.com>
2018-01-10 20:53:57 -0600

    fixed date issue on mobile

--- www/js/component-methods.js
+++ www/js/component-methods.js
@@ -19,12 +19,20 @@ function formatSeconds(sec){
     }
 }
 
+
+function string2Date(str){
+    let d = str.split(' ')
+    let d0 = d[0].split('-')
+    let d1 = d[1].split(':')
+    return new Date(d0[0],d0[1],d0[2],d1[0],d1[1],d1[2])
+}
+
 function printSeen(firstSeen,lastSeen,type){
     if( firstSeen == lastSeen ){
         return `It's the first time seeing this ${type},`
     } else {
-        let f = new Date(firstSeen)
-        let l = new Date(lastSeen)
+        let f = string2Date(firstSeen)
+        let l = string2Date(lastSeen)
         let time = this.formatSeconds( (l-f)/1000 )
         return `This ${type} was last seen ${time} ago,`
     }
