278471c52db318d915d75512aa3c7f0ed53ea41d
Brannon Dorsey <brannon@brannondorsey.com>
2018-03-30 10:09:08 -0500

    Begin inclusion of clouds. Still need to make them dynamically spawned by DNS requests and remove them once offscreen, but this is a start.

--- www/habitat/js/Habitat.js
+++ www/habitat/js/Habitat.js
@@ -42,6 +42,7 @@ class Habitat {
         this.gndHeights = []
         this.butterflies = []
         this.flowers = []
+        this.clouds = []
         // options
         this.bgColor = (c && c.bgColor) ? c.bgColor : 0xffffff
         this.fog = (c && c.fog) ? c.fog : false
@@ -340,6 +341,11 @@ class Habitat {
     }
 
     setupScene(){
+
+        for (let i = 0; i < 10; i++) {
+            setTimeout(() => this.addCloud(), this.ran(0, 5000))
+        }
+
         // camera
         this.camera = new THREE.PerspectiveCamera(
             50, innerWidth/innerHeight, 1, 10000 )
@@ -420,5 +426,76 @@ class Habitat {
         TWEEN.update()
 
         this.renderer.render( this.scene, this.camera )
+    }
+
+    removeDeadClouds() {
+        for(let i = elements.length - 1; i >= 0 ; i--){
+            if(elements[i] == 5){
+                elements.splice(i, 1);
+            }
+        }
+    }
+
+    addCloud(dnsRecord) {
+
+        const manager = new THREE.LoadingManager()
+		manager.onProgress = function (item, loaded, total) {
+			console.log(item, loaded, total)
+		}
+
+		var onProgress = function (xhr) {
+			if ( xhr.lengthComputable ) {
+				var percentComplete = xhr.loaded / xhr.total * 100;
+				console.log( Math.round(percentComplete, 2) + '% downloaded' )
+			}
+		}
+
+		var onError = function (xhr) {
+            console.log(xhr)
+		}
+
+		var loader = new THREE.OBJLoader(manager)
+
+		loader.load(`js/Clouds_Separated/Cloud${this.ran(1, 4, true)}.obj`, cloud => {
+
+            const scale = this.ran(25, 50, true)
+            cloud.scale.x = scale
+            cloud.scale.y = scale
+            cloud.scale.z = scale
+
+			cloud.position.y = this.ran(0, 600)
+            cloud.position.z = this.ran(0, -700)
+            cloud.position.x = -800
+
+            const material = new THREE.MeshPhongMaterial({
+                color: '#ffffff',
+                emissive: '#555555',
+                shininess: 0,
+                flatShading: true
+            })
+
+            cloud.traverse(child => {
+                console.log(child.name)
+				if (child instanceof THREE.Mesh) {
+					child.material = material
+				}
+			})
+
+            this.clouds.push(cloud)
+
+            const speed = this.ran(50, 100, true) * 1000
+            new TWEEN.Tween(cloud.position)
+                .to({ x:800, y:cloud.position.y, z:cloud.position.z }, speed)
+                .start()
+                // .call(() => {
+                //     this.scene.remove(cloud.name)
+                //     cloud.isDead = true
+                // })
+
+            cloud.name = dnsRecord
+            this.scene.add(cloud)
+
+		}, onProgress, onError )
+
     }
 }
